5a7012023ea3b548c5f3ba4fc00801fa53312b3e
Aakansh Anand <aakanshanand15@gmail.com>
2024-12-15 10:59:33 +0530

    Colors, New Features & Indentation improvement

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 It's a basic Todo list file which stores you todos in a `todo.json` file. You can do the following things with the commands.
 
-- Add Task : `node todo.js add "whatever-your-task"`
-- Remove Task : `node todo.js remove 1` where `1` is the index number assigned to task.
-- Update : `node todo.js change <index> <change-you-want>`
-- List all tasks : `node todo.js list`
-- Change status to done : `node todo.js done <index>`
-- Change status to pending : `node todo.js pending <index>`
 
+- Add Task : ```node todo.js add "whatever-your-task"```
+- Remove Task : ```node todo.js remove 1``` where `1` is the index number assigned to task.
+- Update : ```node todo.js change <index> <change-you-want>```
+- List all tasks : ```node todo.js list```
+- List pending tasks : ```node todo.js list-pending```
+- List in-progess tasks : ```node todo.js list-in-progress```
+- List done tasks : ```node todo.js list-done```
+- Change status to done : ```node todo.js done <index>```
+- Change status to pending : ```node todo.js pending <index>```
 ---
 
 ### Note for linux & Mac users (WSL included).
@@ -17,7 +20,10 @@ I understand bash scripts as well, So I turned my `.bashrc` file and made a cust
 
 If you're a mac user, then you don't have `.bashrc` but `.zshrc` so configure accordingly.
 
-### DON'T MESS YOUR BASHRC or ZSHRC.
+---
+Personally I changed ```node todo.js``` with an `alias task="node todo.js"` so understand the syntax of bash and then only apply this kind of thing. I also applied some colors based on your terminal theme, it will look as per you theme so it could be good, bad or normal.
+
+### Don't mess with your .bashrc or .zshrc
 
 Make a clone before you make changes in the original file.
 
@@ -27,3 +33,5 @@ https://github.com/aakansh-anand/terminal-todo
 
 ## Inspired by Roadmap.sh
 https://roadmap.sh/projects/task-tracker
+
+### Enjoy :)
--- todo.js
+++ todo.js
@@ -1,37 +1,5 @@
-/* 
-We are trying to read files and write data on it back using Node.js
-First, we imported the "fs" node module, we used the classic way of
-adding the module, ES6+ ways are also applicable with proper syntax
- 
-Second, we gave the file in which we want to read and write the data
-then we made command and arguments. Command and arguments are used
-with  node.js as runner to run the program we made. 
-(that's how you use bash files as well, commands and arguments are just 
-like flags in bash)
-*/
-
-/* 
-Third, we set the args and commands with process.argv, it is used to add
-the commands and args as flags to be used with node.js. They doesn't work
-explicitly, we have to define them. *commands & args are placeholders only*
-
-Fourth we check the commands with any prefered way of conditionals checks
-and run the required code for the condition. Here we have 3 conditions to
-check, list / add / remove. If user doesn't give any of them, show error.
-
-Fifth, Add the functionalities to the functions you're passing starting 
-with loading everything from the file. To achieve that we're going to use
-try-catch block because there could be a scene where file either doesn't
-exists or it could be secured via some method so we can't read that file.
-
-We read that file using "fs" package "readFileSync" to read the file in a
-synchrounous nature and store it in some variable, This is called "buffer"
-data. Then we have to convert that file data into string using "toString()"
-method in javascript. When you convert the buffer into string, it turns out
-to be JSON format, now we can parse with JSON.parse(dataJSON) to get array
-format.
-*/
 const fs = require("fs");
+const colors = require("colors");
 const filePath = `${__dirname}/todo.json`;
 
 const command = process.argv[2];
@@ -70,16 +38,16 @@ const addTask = (task) => {
 const listTasks = () => {
   const allTasks = loadTasks();
   console.log(
-    "---------------------------------------------------------------"
+    "\t-----------------------------------------------------".green
   );
   if (allTasks.length === 0) {
-    console.log("   No tasks to show\n");
+    console.log("\tNo tasks to show\n");
     return;
   }
   allTasks.forEach((item, index) => {
-    console.log(`   ${index + 1} - ${item.task} : ${item.done}`);
+    console.log(`\t${index + 1} - ${item.task} : ${item.done}`.italic.magenta);
     console.log(
-      "---------------------------------------------------------------"
+      "\t-----------------------------------------------------".cyan
     );
   });
 };
@@ -87,17 +55,17 @@ const listTasks = () => {
 function listDone() {
   const allTasks = loadTasks();
   console.log(
-    "---------------------------------------------------------------"
+    "\t-----------------------------------------------------"
   );
   if (allTasks.length === 0) {
-    console.log("   No tasks to show\n");
+    console.log("\tNo tasks to show\n");
     return;
   }
   allTasks.forEach((item, index) => {
     if (item.done === "Done") {
-      console.log(`   ${index + 1} - ${item.task} : ${item.done}`);
+      console.log(`\t${index + 1} - ${item.task} : ${item.done}`);
       console.log(
-        "---------------------------------------------------------------"
+        "\t-----------------------------------------------------"
       );
     }
   });
@@ -106,17 +74,17 @@ function listDone() {
 function listPending() {
   const allTasks = loadTasks();
   console.log(
-    "---------------------------------------------------------------"
+    "\t-----------------------------------------------------"
   );
   if (allTasks.length === 0) {
-    console.log("   No tasks to show\n");
+    console.log("\tNo tasks to show\n");
     return;
   }
   allTasks.forEach((item, index) => {
     if (item.done === "Pending") {
-      console.log(`   ${index + 1} - ${item.task} : ${item.done}`);
+      console.log(`\t${index + 1} - ${item.task} : ${item.done}`);
       console.log(
-        "---------------------------------------------------------------"
+        "\t-----------------------------------------------------"
       );
     }
   });
@@ -125,17 +93,17 @@ function listPending() {
 function listInProgress() {
   const allTasks = loadTasks();
   console.log(
-    "---------------------------------------------------------------"
+    "\t-----------------------------------------------------"
   );
   if (allTasks.length === 0) {
-    console.log("   No tasks to show\n");
+    console.log("\tNo tasks to show\n");
     return;
   }
   allTasks.forEach((item, index) => {
     if (item.done === "In Progess") {
-      console.log(`   ${index + 1} - ${item.task} : ${item.done}`);
+      console.log(`\t${index + 1} - ${item.task} : ${item.done}`);
       console.log(
-        "---------------------------------------------------------------"
+        "\t-----------------------------------------------------"
       );
     }
   });
@@ -188,13 +156,21 @@ function markInProgress(index) {
   listTasks();
 }
 
+function listAllCommands() {
+  console.log(
+    ` \tlist\n \tlist-in-progress\n \tlist-pending\n \tlist-done\n \tadd\n \tremove\n \tupdate\n \tdone\n \tpending\n \tin-progress\n`
+  );
+}
+
 if (command === "add") {
   addTask(argument);
+} else if (command === "commands") {
+  listAllCommands();
 } else if (command === "list") {
   listTasks();
 } else if (command === "remove") {
   removeTask(parseInt(argument));
-} else if (command === "change") {
+} else if (command === "update") {
   updateTask(parseInt(argument), updater);
 } else if (command === "done") {
   doneTask(parseInt(argument));
